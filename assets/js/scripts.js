f0dcc610d25a7b50e6759829f9726fed7318141c
Andrew Barker <barkeras@mail.uc.edu>
2017-04-08 17:39:55 -0400

    Cachin

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -26,13 +26,21 @@ jQuery(function ($) {
     'use strict';
     
     
-    function onUpdateReady() {
-  console.log('found new version!');
-}
-window.applicationCache.addEventListener('updateready', onUpdateReady);
-if(window.applicationCache.status === window.applicationCache.UPDATEREADY) {
-  onUpdateReady();
-}
+   // Check if a new cache is available on page load.
+//window.addEventListener('load', function(e) {
+//
+//  window.applicationCache.addEventListener('updateready', function(e) {
+//    if (window.applicationCache.status == window.applicationCache.UPDATEREADY) {
+//      // Browser downloaded a new app cache.
+//      if (confirm('A new version of this site is available. Load it?')) {
+//        window.location.reload();
+//      }
+//    } else {
+//      // Manifest didn't changed. Nothing new to server.
+//    }
+//  }, false);
+//
+//}, false);
     
 
     /* ---------------------------------------------- /*
